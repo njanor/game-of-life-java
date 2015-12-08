bdc223e0ac3550d8f298211c332aca241997dd0a
Njål Nordmark <njaal.nordmark@gmail.com>
2015-12-08 05:42:01 +0100

    Initial commit with simple Java/Gradle project

---
+++ src/main/java/no/njanor/gameoflife/consolerunner/Runner.java
@@ -0,0 +1,7 @@
+package no.njanor.gameoflife.consolerunner;
+
+public class Runner {
+    public static void main(String args[]) {
+        System.out.println("Starting a new Game of Life...");
+    }
+}
