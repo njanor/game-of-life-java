156c816df0050bb622d8426312aa8ca6ea47f266
Njål Nordmark <njaal.nordmark@gmail.com>
2015-12-10 06:11:05 +0100

    CoordinateIntepreter::fromString handles several, comma separated coordinates

--- src/main/java/no/njanor/gameoflife/consolerunner/CoordinateInterpreter.java
+++ src/main/java/no/njanor/gameoflife/consolerunner/CoordinateInterpreter.java
@@ -6,11 +6,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CoordinateInterpreter {
-    public static List<Coordinate> fromString(String input) {
-        if (input == null || input.isEmpty())
-            return new ArrayList<>();
-        List<Coordinate> coordinates = new ArrayList<>();
-        coordinates.add(Coordinate.fromString(input));
-        return coordinates;
+    public static List<Coordinate> fromString(final String input) {
+        return getRemainingCoordinates(new ArrayList<>(), input);
+    }
+
+    private static List<Coordinate> getRemainingCoordinates(List<Coordinate> coordinates, final String input) {
+        if (input == null || input.trim().isEmpty())
+            return coordinates;
+        String trimmedInput = trimInput(input);
+
+        int indexOfCharacterAfterClosingParentheses = trimmedInput.indexOf(')') + 1;
+        String nextCoordinate = trimmedInput.substring(0, indexOfCharacterAfterClosingParentheses);
+        String remainingCoordinates = trimmedInput.substring(indexOfCharacterAfterClosingParentheses);
+        coordinates.add(Coordinate.fromString(nextCoordinate));
+        return getRemainingCoordinates(coordinates, remainingCoordinates);
+    }
+
+    private static String trimInput(String input) {
+        String trimmedInput;
+        if (input.charAt(0) == ',')
+            trimmedInput = input.substring(1);
+        else
+            trimmedInput = input;
+        trimmedInput = trimmedInput.trim();
+        return trimmedInput;
     }
 }
--- src/test/java/no/njanor/gameoflife/consolerunner/CoordinateInterpreterTests.java
+++ src/test/java/no/njanor/gameoflife/consolerunner/CoordinateInterpreterTests.java
@@ -26,6 +26,7 @@ public class CoordinateInterpreterTests {
     @Test
     public void fromString_stringHasOnePositiveCoordinateInIt_returnsListWithCorrectCoordinate() {
         Coordinate coordinate = new Coordinate(1, 3);
+
         List<Coordinate> coordinates = CoordinateInterpreter.fromString(coordinate.toString());
 
         assertEquals(1, coordinates.size());
@@ -35,9 +36,22 @@ public class CoordinateInterpreterTests {
     @Test
     public void fromString_stringHasOneNegativeCoordinateInIt_returnsListWithCorrectCoordinate() {
         Coordinate coordinate = new Coordinate(-1, -20);
+
         List<Coordinate> coordinates = CoordinateInterpreter.fromString(coordinate.toString());
 
         assertEquals(1, coordinates.size());
         assertTrue(coordinates.contains(coordinate));
+    }
+
+    @Test
+    public void fromString_stringHasTwoCoordinatesInIt_returnsListWithCorrectCoordinates() {
+        Coordinate firstCoordinate = new Coordinate(12, 34);
+        Coordinate secondCoordinate = new Coordinate(-4, -1204);
+
+        List<Coordinate> coordinates = CoordinateInterpreter.fromString(firstCoordinate + ", " + secondCoordinate);
+
+        assertEquals(2, coordinates.size());
+        assertTrue(coordinates.contains(firstCoordinate));
+        assertTrue(coordinates.contains(secondCoordinate));
     }
 }
